b84f805ce496a924761160c492a877d291ef3030
Jim Wilson <19673913+RoyHinkley@users.noreply.github.com>
2026-08-30 16:07:39 -0700

    Remove obsolete ActionPlan compiler interface

--- src/Berries.Core/Planning/IActionPlanCompiler.cs
+++
@@ -1,9 +0,0 @@
-using Berries.Core.Decisions;
-using Berries.Core.Domain;
-
-namespace Berries.Core.Planning;
-
-public interface IActionPlanCompiler
-{
-    ActionPlan Compile(Portrait portrait, Disposition disposition);
-}
